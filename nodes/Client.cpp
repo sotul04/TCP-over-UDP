6f8ae4d848f2cc818a59fa9e0a9716a99466dd55
sotul04 <suthasoma04@gmail.com>
2024-12-05 15:08:10 +0700

    fix: file saving

--- nodes/Client.cpp
+++ nodes/Client.cpp
@@ -28,7 +28,8 @@ void Client::run()
         }
         else // file input
         {   
-            string outputPath = "output_"+metadata.first+"."+metadata.second;
+            string outputPath = "output_"+metadata.first;
+            if (!metadata.second.empty()) outputPath += "."+metadata.second;
             result.first.pop_back();
             receiveFile(result.first, outputPath);
             cout << "Received file saved "<< outputPath << endl;
@@ -38,49 +39,3 @@ void Client::run()
     connection->close();
     exit(0);
 }
-
-/*
-#include "node.hpp"
-
-class Client: public Node {
-private:
-    string destIP;
-    uint16_t destPort;
-public:
-    Client(string ip, uint16_t port, string destIP, uint16_t destPort): Node(ip, port) {
-        this->destIP = destIP;
-        this->destPort = destPort;
-    }
-
-    void run() {
-        connection->start();
-
-        // try to send request to broadcast
-        cout << "Trying to contact the sender at " << destIP << ":" << destPort << endl;
-        Connection cont =  connection->seekBroadcast(destIP, destPort);
-
-        Connection status = connection->reqHandShake(cont.ip, cont.port);
-        cout << status.cont << endl;
-        if (status.cont) {
-            pair<vector<Segment>, Connection> result = connection->receiveData(status.ip, status.port, status.seqNum);
-            cout << "SEQNUM: " << result.second.seqNum << endl;
-            connection->accClosing(result.second.ip, result.second.port, result.second.seqNum);
-            cout << "ENDED" << endl;
-            string combined = combineAsString(result.first);
-            cout << "Size: " << combined.size() << endl;
-            cout << combined << endl;
-        }
-        connection->close();
-    }
-
-    void handleMessage(void *buffer) override {
-        cout << "noting" << endl;
-    }
-};
-
-int main() {
-    Client client("172.31.90.136", 9090, "255.255.255.255", 8080);
-    client.run();
-}
-
-*/
